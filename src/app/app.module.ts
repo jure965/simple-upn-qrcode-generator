e4a622c8d274dbcf57b39ced0f7adaae1c755361
Jure <jure.vengust@gmail.com>
2024-04-13 21:17:20 +0200

    use new material modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,13 @@ import {AppComponent} from './app.component';
 import {UpnFormComponent} from './upn-form/upn-form.component';
 import {UpnQrcodeComponent} from './upn-qrcode/upn-qrcode.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatLegacyFormFieldModule as MatFormFieldModule} from '@angular/material/legacy-form-field';
-import {MatLegacyInputModule as MatInputModule} from '@angular/material/legacy-input';
 import {ReactiveFormsModule} from '@angular/forms';
-import {MatLegacyButtonModule as MatButtonModule} from '@angular/material/legacy-button';
 import {NgxQRCodeModule} from '@techiediaries/ngx-qrcode';
-import {MatLegacyCheckboxModule as MatCheckboxModule} from '@angular/material/legacy-checkbox';
 import {NgxWebstorageModule} from 'ngx-webstorage';
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {MatButtonModule} from "@angular/material/button";
+import {MatCheckboxModule} from "@angular/material/checkbox";
 
 @NgModule({
   declarations: [
