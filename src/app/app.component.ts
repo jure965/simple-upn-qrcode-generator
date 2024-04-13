3ad4db3729876fae0f816b4455400b55ba1cebda
Jure <jure.vengust@gmail.com>
2024-04-13 23:38:32 +0200

    migrate modules to standalone

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import {Component, OnInit} from '@angular/core';
+import { QRCodeModule } from 'angularx-qrcode';
+import { UpnFormComponent } from './upn-form/upn-form.component';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    standalone: true,
+    imports: [UpnFormComponent, QRCodeModule],
 })
 export class AppComponent implements OnInit {
   title = 'simple-upn-qrcode-generator';
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,27 @@
-import { enableProdMode } from '@angular/core';
+import { enableProdMode, importProvidersFrom } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
+
 import { environment } from './environments/environment';
+import { AppComponent } from './app/app.component';
+import { NgxWebstorageModule } from 'ngx-webstorage';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { QRCodeModule } from 'angularx-qrcode';
+import { MatButtonModule } from '@angular/material/button';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+bootstrapApplication(AppComponent, {
+    providers: [
+        importProvidersFrom(BrowserModule, MatFormFieldModule, MatInputModule, ReactiveFormsModule, MatButtonModule, QRCodeModule, MatCheckboxModule, NgxWebstorageModule.forRoot()),
+        provideAnimations()
+    ]
+})
   .catch(err => console.error(err));
